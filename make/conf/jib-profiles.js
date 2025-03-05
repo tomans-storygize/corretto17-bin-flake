8ff7bff5864271b4d0e187d70c296c6c81424f60
Thomas Omans <tomans@storygize.com>
2025-03-05 14:28:21 -0800

    Try to use system SETFILE for darwin build

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -448,7 +448,10 @@ var getJibProfilesProfiles = function (input, common, data) {
             target_cpu: "aarch64",
             dependencies: ["devkit", "gtest", "pandoc"],
             configure_args: concat(common.configure_args_64bit,
-                "--with-macosx-version-max=11.00.00"),
+                "--with-macosx-version-max=11.00.00",
+                // Use system SetFile instead of the one in the devkit as the
+                // devkit one may not work on Catalina.
+                "SETFILE=/usr/bin/SetFile"),
         },
 
         "windows-x64": {
